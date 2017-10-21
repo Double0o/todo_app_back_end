208523147abf7ab72066d9d7115ac42c16579365
Marcus Freeman <marcus.freeman@epicacademy.org>
2017-10-21 19:28:16 +0000

    more conditions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,11 +2,21 @@ class TodoController < ApplicationController
 def index
 end
 def show
+    todo_id= params[:id]
+    if todo_id =='1'
+    
     @todo_description = "make the curriculum"
 @todo_pomodoro_estimate=4
-   
-end
-@todo_description = "make the curriculum"
+elsif todo_id =='2'
+    
+    @todo_description = "buy workshop supplies"
 @todo_pomodoro_estimate=4
+
+elsif todo_id =='3'
+    
+    @todo_description = "wash the dishes"
+@todo_pomodoro_estimate=2
+end
+end
 
 end
