3243882857a7f747d15abaab6e9d0de7bf181b4a
Marcus Freeman <marcus.freeman@epicacademy.org>
2017-10-21 18:06:32 +0000

    variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,5 +2,11 @@ class TodoController < ApplicationController
 def index
 end
 def show
+    @todo_description = "make the curriculum"
+@todo_pomodoro_estimate=4
+   
 end
+@todo_description = "make the curriculum"
+@todo_pomodoro_estimate=4
+
 end
