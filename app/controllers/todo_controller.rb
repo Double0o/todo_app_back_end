375e7a88df141b44645aee4dec84aa09fc5b947d
Marcus Freeman <marcus.freeman@epicacademy.org>
2017-10-22 16:23:17 +0000

    removed database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,21 +2,7 @@ class TodoController < ApplicationController
 def index
 end
 def show
-    todo_id= params[:id]
-    if todo_id =='1'
-    
-    @todo_description = "make the curriculum"
-@todo_pomodoro_estimate=4
-elsif todo_id =='2'
-    
-    @todo_description = "buy workshop supplies"
-@todo_pomodoro_estimate=4
-
-elsif todo_id =='3'
-    
-    @todo_description = "wash the dishes"
-@todo_pomodoro_estimate=2
-end
+    @todo = Todo.find_by_id(params[:id])
 end
 
 end
